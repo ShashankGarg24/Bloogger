7b547b11b487d0d9ae908620d175cd0d5e88471e
ShashankGarg24 <sg24101999@gmail.com>
2023-09-10 22:19:17 +0530

    cors config modified

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cors = require('cors')
+const cors = require("cors");
 const blogRouting = require('./routes/blogs')
 const db = require('./db')
 const app = express()
@@ -9,6 +9,11 @@ app.listen(8080, ()=>{
     console.log("Server Started at port 8080....")
 })
 
-app.use(cors())
+const corsOptions ={
+   origin:'*', 
+   credentials:true,
+   optionSuccessStatus:200,
+}
+app.use(cors(corsOptions)) 
 app.use(express.json())
 app.use('/blogs', blogRouting)
