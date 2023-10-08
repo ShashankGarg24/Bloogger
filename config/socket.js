e553fcf2ed3b5715a4cb5168f49e9e11d99fdc20
ShashankGarg24 <sg24101999@gmail.com>
2023-10-09 03:51:30 +0530

    imports removed

--- config/socket.js
+++ config/socket.js
@@ -11,18 +11,14 @@ const app = express();
 const server = http.createServer(app);
 
 const io = require("socket.io")(server, {
-    handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, 
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
+    cors: {
+      origin: "https://blooggerr.netlify.app/",
+      methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+      credentialS: true
     }
+   
 });
 
-server.listen(8080)
  var onlineUserDictionary = {}
 
  const addOnlineUser =(userId, socketId)=>{
