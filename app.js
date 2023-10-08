d7964f80b440f893f4e7030996abc0970a3fc52e
ShashankGarg24 <sg24101999@gmail.com>
2023-10-09 04:13:30 +0530

    socket modified

--- app.js
+++ app.js
@@ -11,8 +11,56 @@ const db = require('./config/db');
 const socket = require('./config/socket');
 const app = express()
 
+const express = require("express");
+const http = require("http");
 
-app.listen(8080, ()=>{
+const server = http.createServer(app);
+
+const io = require("socket.io")(server, {
+    cors: {
+      origin: "https://blooggerr.netlify.app",
+      methods: ["GET", "POST", "PATCH", "PUT", "DELETE"]
+    }
+   
+});
+
+ var onlineUserDictionary = {}
+
+ const addOnlineUser =(userId, socketId)=>{
+    console.log("New user added to socket")
+    onlineUserDictionary[userId] = socketId
+ }
+
+ const getOnlineUser =(userId)=>{
+    return onlineUserDictionary[userId]
+ }
+
+ const deleteOnlineUser =(userId)=>{
+    delete onlineUserDictionary[userId]
+ }
+io.on("connection", (socket) => {
+  socket.on("addNewOnlineUser", (userId)=>{
+    addOnlineUser(userId, socket.id)
+    console.log(onlineUserDictionary)
+    io.to(socket.id).emit("greet", "You are connected " + userId)
+  })
+  socket.on("disconnect", (userId)=>{
+    deleteOnlineUser(userId)
+    console.log(onlineUserDictionary)
+    console.log("bye")
+  })
+});
+
+
+const sendNotificationToUser = (senderId, receiverId, blogId, type) =>{
+    if(senderId, receiverId, blogId){
+        io.to(onlineUserDictionary[receiverId]).emit("notifications", ({senderId, blogId, type}))
+    }
+ }
+
+
+
+server.listen(8080, ()=>{
     console.log("Server Started at port 8080....")
 })
 
