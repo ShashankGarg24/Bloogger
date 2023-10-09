d5665774e93cfec4fe832576f9d283436595e36a
ShashankGarg24 <sg24101999@gmail.com>
2023-10-09 06:27:47 +0530

    added event emitter

--- services/blogService.js
+++ services/blogService.js
@@ -9,7 +9,7 @@ const { getBlogsForCardsFrom } = require('../utilities/blogUtilities')
 const {sendNotificationToUser}  = require('../app')
 const Comment = require('../models/Comment')
 const events = require('events');
-export const eventEmitter = new events.EventEmitter();
+const eventEmitter = new events.EventEmitter();
 
 
 const router = express.Router()
@@ -206,4 +206,4 @@ router.delete('/delete/:id', jwtAuth, async(req,res) =>{
     
 })
 
-module.exports = router
+module.exports = {router, eventEmitter}
