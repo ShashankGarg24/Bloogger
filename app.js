1f6c70cb00ff839105d2eb835f93e90a76b20819
ShashankGarg24 <sg24101999@gmail.com>
2023-10-09 06:00:12 +0530

    functions for sockets modified

--- app.js
+++ app.js
@@ -1,17 +1,53 @@
 const express = require('express')
 const cors = require("cors");
 const cookieParser = require('cookie-parser')
-const blogRouting = require('./services/blogService')
-const categoryRouting = require('./services/admin/categoryService')
-const loginService = require('./services/auth/loginService')
-const registerService = require('./services/auth/registerService')
-const userService = require('./services/userService')
-const commentRouting = require('./services/commentService')
 const db = require('./config/db');
-// const socket = require('./config/socket');
 const app = express()
 const http = require("http");
 const server = http.createServer(app);
+var onlineUserDictionary = {}
+
+const io = require("socket.io")(server, {
+    cors: {
+      origin: "https://blooggerr.netlify.app",
+      methods: ["GET", "POST", "PATCH", "PUT", "DELETE"]
+    }
+   
+});
+
+
+ const addOnlineUser =(userId, socketId)=>{
+    console.log("New user added to socket")
+    onlineUserDictionary[userId] = socketId
+ }
+
+ const getOnlineUser =(userId)=>{
+    return onlineUserDictionary[userId]
+ }
+
+ const deleteOnlineUser =(userId)=>{
+    delete onlineUserDictionary[userId]
+ }
+io.on("connection", (socket) => {
+  socket.on("addNewOnlineUser", (userId)=>{
+    addOnlineUser(userId, socket.id)
+    console.log(onlineUserDictionary)
+    io.to(socket.id).emit("greet", "You are connected " + userId)
+  })
+  socket.on("disconnect", (userId)=>{
+    deleteOnlineUser(userId)
+    console.log(onlineUserDictionary)
+    console.log("bye")
+  })
+});
+
+
+const sendNotificationToUser = (senderId, receiverId, blogId, type) =>{
+    if(senderId, receiverId, blogId){
+        io.to(onlineUserDictionary[receiverId]).emit("notifications", ({senderId, blogId, type}))
+    }
+ }
+
 
 
 server.listen(8080, ()=>{
@@ -26,11 +62,5 @@ const corsOptions ={
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
-app.use('/register', registerService)
-app.use('/login', loginService)
-app.use('/blogs', blogRouting)
-app.use('/account', userService)
-app.use('/admin/category', categoryRouting)
-app.use('/comment', commentRouting)
 
-exports.server = server
+module.exports = {sendNotificationToUser}
--- config/socket.js
+++ config/socket.js
@@ -1,47 +1,56 @@
-var onlineUserDictionary = {}
-const app = require("../app")
-console.log(app)
-console.log(app.server)
+// // var express = require("express"),
+// // app = express(),
+// // server = require('http').createServer(app),
+// // io = require("socket.io").listen(server);
+// // io.origins("*:*")
+// // server.listen(3000)
+// const express = require("express");
+// const http = require("http");
 
-const io = require("socket.io")(app.server, {
-   cors: {
-     origin: "https://blooggerr.netlify.app",
-     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"]
-   }
-  
-});
+// const app = express();
+// const server = http.createServer(app);
+
+// const io = require("socket.io")(server, {
+//     cors: {
+//       origin: "https://blooggerr.netlify.app",
+//       methods: ["GET", "POST", "PATCH", "PUT", "DELETE"]
+//     }
+   
+// });
+
+//  var onlineUserDictionary = {}
+
+//  const addOnlineUser =(userId, socketId)=>{
+//     console.log("New user added to socket")
+//     onlineUserDictionary[userId] = socketId
+//  }
+
+//  const getOnlineUser =(userId)=>{
+//     return onlineUserDictionary[userId]
+//  }
+
+//  const deleteOnlineUser =(userId)=>{
+//     delete onlineUserDictionary[userId]
+//  }
+// io.on("connection", (socket) => {
+//   socket.on("addNewOnlineUser", (userId)=>{
+//     addOnlineUser(userId, socket.id)
+//     console.log(onlineUserDictionary)
+//     io.to(socket.id).emit("greet", "You are connected " + userId)
+//   })
+//   socket.on("disconnect", (userId)=>{
+//     deleteOnlineUser(userId)
+//     console.log(onlineUserDictionary)
+//     console.log("bye")
+//   })
+// });
 
 
-const addOnlineUser =(userId, socketId)=>{
-   console.log("New user added to socket")
-   onlineUserDictionary[userId] = socketId
-}
-
-const getOnlineUser =(userId)=>{
-   return onlineUserDictionary[userId]
-}
-
-const deleteOnlineUser =(userId)=>{
-   delete onlineUserDictionary[userId]
-}
-io.on("connection", (socket) => {
- socket.on("addNewOnlineUser", (userId)=>{
-   addOnlineUser(userId, socket.id)
-   console.log(onlineUserDictionary)
-   io.to(socket.id).emit("greet", "You are connected " + userId)
- })
- socket.on("disconnect", (userId)=>{
-   deleteOnlineUser(userId)
-   console.log(onlineUserDictionary)
-   console.log("bye")
- })
-});
+// const sendNotificationToUser = (senderId, receiverId, blogId, type) =>{
+//     if(senderId, receiverId, blogId){
+//         io.to(onlineUserDictionary[receiverId]).emit("notifications", ({senderId, blogId, type}))
+//     }
+//  }
 
 
-const sendNotificationToUser = (senderId, receiverId, blogId, type) =>{
-   if(senderId, receiverId, blogId){
-       io.to(onlineUserDictionary[receiverId]).emit("notifications", ({senderId, blogId, type}))
-   }
-}
-
-module.exports = {sendNotificationToUser}
+// module.exports = {onlineUserDictionary, sendNotificationToUser}
